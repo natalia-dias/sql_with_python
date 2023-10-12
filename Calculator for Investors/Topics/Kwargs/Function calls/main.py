12bf808a3300435a1587996797a6ceed46ac6f3f
Natalia Dias <nataliadias.pt@gmail.com>
2023-10-12 15:26:01 +0100

    daily challenges

--- Calculator for Investors/Topics/Kwargs/Function calls/main.py
+++ Calculator for Investors/Topics/Kwargs/Function calls/main.py
@@ -1,2 +1,7 @@
-#  You can experiment here, it won’t be checked
-
+import pandas as pd
+source = '/Users/ndias/Downloads/hyperskill-dataset-87937638.txt'
+data = pd.read_csv(source)
+print(data.head())
+# data.
+print('____')
+print(data.set_index('Name').head(10))
